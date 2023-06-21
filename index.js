a3cb190ee641a43bc1f119cb49febb2b71cf2c73
akorede <emiolasaheed1@gmail.com>
2023-06-21 15:57:18 +0100

    Fix stylelint error

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const navLink = document.querySelector('.nav-link');
 const hamburgerIcon = document.querySelector('.toolbar-hamburger');
 const closeIcon = document.querySelector('.close-container');
 const headerHeroSection = document.querySelector('.header-hero-section');
-
 hamburgerIcon.addEventListener('click', () => {
   navLink.classList.add('show-link');
   headerHeroSection.classList.add('blur-header-hero-section');
