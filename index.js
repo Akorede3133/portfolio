e5a0c4db26d4973143033a38c952df8c6e4a1608
akorede <emiolasaheed1@gmail.com>
2023-06-22 08:12:34 +0100

    Implement popup window functionality

--- index.js
+++ index.js
@@ -4,38 +4,43 @@ const closeIcon = document.querySelector('.close-container');
 const links = document.querySelectorAll('.nav-link > li');
 const headerHeroSection = document.querySelector('.header-hero-section');
 const projectSection = document.querySelector('.project-section');
+const popupSection = document.querySelector('.pop-up');
 const projects = [
   {
     name: 'tonic',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     image: 'images/tonic.png',
     technologies: ['HTML', 'CSS', 'javascript'],
-    liveLink: 'https://www.tonicLive.com',
-    sourceLink: 'https://www.github.com/Akorede3133',
+    liveLink: 'https://www.google.com',
+    sourceLink: 'https://www.github.com/Akorede3133/portfolio',
+    id: 'tonic',
   },
   {
-    name: 'multi-Post strories',
+    name: 'multi-Post stories',
     description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
     image: 'images/multi-stories.png',
     technologies: ['HTML', 'Ruby on rails', 'CSS', 'javascript'],
-    liveLink: 'https://www.muli-post.com',
-    sourceLink: 'https://www.github.com/Akorede3133',
+    liveLink: 'https://www.google.com',
+    sourceLink: 'https://www.github.com/Akorede3133/portfolio',
+    id: 'stories',
   },
   {
     name: 'facebook 360',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     image: 'images/facebook360.png',
     technologies: ['HTML', 'CSS', 'javascript'],
-    liveLink: 'https://www.tonicLive.com',
-    sourceLink: 'https://www.github.com/Akorede3133',
+    liveLink: 'https://www.google.com',
+    sourceLink: 'https://www.github.com/Akorede3133/portfolio',
+    id: 'facebook',
   },
   {
     name: 'uber navigation',
     description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
     image: 'images/uber.png',
     technologies: ['HTML', 'Ruby on rails', 'CSS', 'javascript'],
-    liveLink: 'https://www.uber.com',
-    sourceLink: 'https://www.github.com/Akorede3133',
+    liveLink: 'https://www.google.com',
+    sourceLink: 'https://www.github.com/Akorede3133/portfolio',
+    id: 'uber',
   },
 ];
 hamburgerIcon.addEventListener('click', () => {
@@ -76,10 +81,71 @@ const projectElements = projects.map((project) => (
   )).join('')
   }
       </ul>
-      <button>
+      <button id=${project.id}>
           See Project
       </button>
   </div>
 </article>`
 )).join('');
 projectSection.insertAdjacentHTML('beforeend', projectElements);
+const seeProjectBtns = projectSection.querySelectorAll('.project-description button');
+function showPopUp(btn) {
+  const objElement = projects.find((project) => project.id === btn.id);
+  const element = ` <div class="overlay">
+  <div class="head">
+      <h2>${objElement.name}</h2>
+      <img src="images/pop-up-close-icon.png" class="pop-up-close-icon" alt="">
+  </div>
+ <div class="project-description">
+      <ul class="experience-list">
+          <li>CANOPY</li>
+          <li>Backend Dev</li>
+          <li>2015</li>
+      </ul>
+      <img class="detail-img" src=${objElement.image} alt="">
+ </div>
+ <div class="flex-pop">
+  <div class="pop-up-desc">
+      <p>
+        ${objElement.description}
+      </p>
+  </div>
+  <div class="side-pop">
+      <ul class="skill-list">
+      ${
+  objElement.technologies.map((technology) => (
+    `<li>${technology}</li>`
+  )).join('')
+}
+      </ul>
+      <div class="see-container">
+        <a href=${objElement.liveLink}>
+          <button>
+            <span>see live</span>
+            <img src="images/see-live-icon.png" alt="">
+          </button>
+        </a>
+      <a href=${objElement.sourceLink}>
+        <button>
+          <span>see source</span>
+          <img src="images/see-source-icon.png" alt="">
+        </button>
+      </a>
+    </div>
+  </div>
+ </div>
+</div>`;
+  popupSection.insertAdjacentHTML('beforeend', element);
+}
+seeProjectBtns.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    showPopUp(btn);
+    popupSection.classList.add('show-popup');
+    window.scrollTo(0, 0);
+    const closePopBtn = popupSection.querySelector('.pop-up-close-icon');
+    closePopBtn.addEventListener('click', () => {
+      popupSection.classList.remove('show-popup');
+      popupSection.innerHTML = '';
+    });
+  });
+});
